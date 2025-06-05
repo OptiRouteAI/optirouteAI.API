fcf2adfd42ee048284124aac3c4ff0177bd74c61
Fabricio Huillca <u202118067@upc.edu.pe>
2025-06-05 12:03:24 -0500

    feat: create module inventory

---
+++ app/Inventory/Model/saldo_ubicacion.py
@@ -0,0 +1,14 @@
+from sqlalchemy import Column, String, Integer
+from app.database import Base
+
+class SaldoUbicacion(Base):
+    __tablename__ = "saldo_ubicacion"
+
+    id_saldo_ubic = Column(Integer, primary_key=True, autoincrement=True)
+    cod_articulo = Column(String(50), nullable=False)
+    ubicacion = Column(String(50), nullable=False)
+    um = Column(String(3), nullable=False)
+    cantidad = Column(Integer, nullable=False, default=0)
+    cantidad_reservada = Column(Integer, nullable=False, default=0)
+    cod_lpn = Column(String(50),nullable=False)
+    secuencia = Column(Integer, nullable=False, default=0)
