3d88d0fe9189521b7004c4fd2367c6ddfeea4eba
Fabricio Huillca <u202118067@upc.edu.pe>
2025-06-06 15:45:34 -0500

    feat: update purchase controller

--- app/PurchaseOrder/Controller/controllers.py
+++ app/PurchaseOrder/Controller/controllers.py
@@ -1,8 +1,10 @@
-from fastapi import APIRouter, Depends, status
+from fastapi import APIRouter, Depends, status, Query
+from datetime import date
 from sqlalchemy.orm import Session
 from app.database import get_db
-from app.PurchaseOrder.Schemas.purchase_schema import PedidoEntradaSchema, PedidoSalidaSchema
-from app.PurchaseOrder.Services.purchase_service import procesar_nuevo_pedido, obtener_pedidos
+from app.PurchaseOrder.Schemas.purchase_schema import PedidoEntradaSchema, PedidoSalidaSchema, PedidoFiltroSchema, PedidoDetSalidaSchema
+from app.PurchaseOrder.Services.purchase_service import procesar_nuevo_pedido, obtener_pedidos, filtrar_pedidos, obtener_detalles_pedido
+from typing import List, Optional
 
 router = APIRouter(prefix="/purchase-order", tags=["Purchase Order"])
 
@@ -13,7 +15,7 @@ def crear_pedido(
 ):
     nuevo_pedido = procesar_nuevo_pedido(
         db,
-        pedido.nro_pedido,   #ahora se incluye el nro_pedido
+        pedido.nro_pedido,
         pedido.cliente,
         pedido.direccion,
         [d.dict() for d in pedido.detalles]
@@ -24,3 +26,25 @@ def crear_pedido(
 def listar_pedidos(db: Session = Depends(get_db)):
     pedidos = obtener_pedidos(db)
     return [PedidoSalidaSchema.from_orm(p) for p in pedidos]
+
+@router.get("/filtrar", response_model=List[PedidoFiltroSchema])  # puedes cambiar a un schema de salida si quieres
+def buscar_pedidos(
+    nro_pedido: Optional[str] = Query(None),
+    cliente: Optional[str] = Query(None),
+    fecha: Optional[date] = Query(None),
+    db: Session = Depends(get_db)
+):
+    return filtrar_pedidos(db, nro_pedido, cliente, fecha)
+
+@router.get("/{nro_pedido}/details", response_model=List[PedidoDetSalidaSchema])
+def obtener_detalles_de_pedido(nro_pedido: str, db: Session = Depends(get_db)):
+    detalles = obtener_detalles_pedido(db, nro_pedido)
+    return [
+        PedidoDetSalidaSchema(
+            cod_articulo=d.cod_articulo,
+            descripcion=d.descripcion,
+            cantidad=d.cantidad,
+            UM=d.UM
+        )
+        for d in detalles
+    ]
