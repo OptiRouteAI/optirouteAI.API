fcfb572c688861e48c1339a39becde314004caa3
Fabricio Huillca <u202118067@upc.edu.pe>
2025-06-11 11:07:29 -0500

    feat: create configuration router

--- app/main.py
+++ app/main.py
@@ -1,19 +1,16 @@
 from fastapi import FastAPI
 from app.database import engine, Base
-
-import app.PurchaseOrder.Model.purchase_order  
-import app.Picking.Model.picking_order         
-import app.Inventory.Model.saldo_ubicacion          
-import app.Profile.Model.User
-
+from sqlalchemy import event
+from app.PurchaseOrder.Controller.controllers import router as purchase_router
+from app.Picking.Controller.picking_controller import router as picking_router
+from app.Profile.Controller.user_controller import router as user_router
+from app.Configuration.Controller.configuration_controller import router as configuration_router
+from app.database import engine, Base, SessionLocal
 Base.metadata.create_all(bind=engine)
 
 print("Tablas registradas:", Base.metadata.tables.keys())
 
 
-from app.PurchaseOrder.Controller.controllers import router as purchase_router
-from app.Picking.Controller.picking_controller import router as picking_router
-from app.Profile.Controller.user_controller import router as user_router
 
 app = FastAPI(
     title="OptiRoute AI API",
@@ -24,3 +21,4 @@ app = FastAPI(
 app.include_router(purchase_router, prefix="/purchase")
 app.include_router(picking_router, prefix="/picking")
 app.include_router(user_router, prefix="/users")
+app.include_router(configuration_router, prefix="/configuration")
