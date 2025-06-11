8644aacb35263fa08fb54e7508f1f4838d89889b
Fabricio Huillca <u202118067@upc.edu.pe>
2025-06-11 15:59:43 -0500

    feat: added IA_PICKING

--- app/Picking/Services/picking_service.py
+++ app/Picking/Services/picking_service.py
@@ -1,9 +1,9 @@
 from sqlalchemy.orm import Session
-from app.Picking.Implementation.picking_imp import generar_picking_tradicional
+from app.Picking.Implementation.picking_imp import generar_picking_tradicional, generar_picking_con_ia
 from app.Picking.Model.picking_order import PickingCab
 
 def crear_picking(db: Session, pedidos: list[str]):
-    return generar_picking_tradicional(db, pedidos)
+    return generar_picking_con_ia(db, pedidos)
 
 
 def listar_picking_cabecera(db: Session):
