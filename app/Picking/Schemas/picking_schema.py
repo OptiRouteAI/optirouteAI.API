81fe9d44835f71503e911f26df1e417b8666f53b
Fabricio Huillca <u202118067@upc.edu.pe>
2025-06-12 14:12:14 -0500

    feat: added PickingRuta

--- app/Picking/Schemas/picking_schema.py
+++ app/Picking/Schemas/picking_schema.py
@@ -24,3 +24,18 @@ class PickingCabeceraSchema(BaseModel):
     nro_picking: str
     fecha_generacion: datetime
     estado: str
+
+class PickingRutaDetalleSchema(BaseModel):
+    cod_articulo: str
+    descripcion: str
+    cantidad: int
+    um: str
+    ubicacion: str
+
+class PickingRutaCabeceraSchema(BaseModel):
+    nro_pedido: str
+    cliente: str
+    detalles: List[PickingRutaDetalleSchema]
+
+class PickingRutaAgrupadaSalidaSchema(BaseModel):
+    rutas: List[PickingRutaCabeceraSchema]
